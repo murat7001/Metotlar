e0eedeb9641ca26fab053f99c5a32cabf8528c14
Murat Yıldırım <2020555068@ogr.cu.edu.tr>
2021-07-23 19:16:07 +0300

    Add project files.

---
+++ SepetManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        //naming convention
+        public void Ekle(Urun urun
+            )
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi : " + urun.UrununAdi);
+        }
+
+        public void Ekle2(string urununAdi,string aciklama, double urununFiyati)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi : " + urununAdi);
+        }
+
+    }
+}
---
+++ Urun.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class Urun
+    {
+        //property-özellik
+        public string Id { get; set; }
+        public string UrununAdi { get; set; }
+        public double UrununFiyati { get; set; }
+        public string Aciklama { get; set; }
+    }
+}
